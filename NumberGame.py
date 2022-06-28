b287a32c6d486a7b1fdda4bf763d06d8f5e30982
funith <funisuke2222@gmail.com>
2022-06-29 02:58:41 +0900

    Setting a limit on the number of times

--- NumberGame.py
+++ NumberGame.py
@@ -1,5 +1,8 @@
+from operator import length_hint
 import random
-random.randint(100, 999)
+
+from scipy import rand
+answer = random.randint(100, 999)
 def generator(length):
     ans = []
     while len(ans) < length:
@@ -23,12 +26,10 @@ def hitblow(command, ans):
     return [hit, blow]
 
 
-
-
 print("[Hit & Blow Game] \n")
 print("指定された桁数の数字において．数当てゲームを行います")
-print("各桁ごとに, 桁も数値も当てられれば「Hit」,桁が異なり数値だけが当てられれば「Blow」と表示されます ")
-print("間違えるたびに1つずつヒントが与えられます")
+print("各桁ごとに 桁も数値も当てられれば「Hit, 桁が異なり数値だけが当てられれば「Blow」と表示されます ")
+print("間違えるたびに1つずつヒントが与えられます 5回以内に当てられないとGame Over!")
 print("できるだけ少ない試行回数で当てられるよう頑張りましょう!!! \n")
 
 digit = input("遊びたい桁数を入力して下さい: ")
@@ -45,3 +46,10 @@ while cont:
     if hit == len(ans):
         print("おめでとうございます!正解です!!! You took {} steps.".format(count))
         cont = False
+
+    if count > 4:
+            print("Game Over!!!!!")
+            print("正解は%dです!!!" %answer)
+            break
+        
+
